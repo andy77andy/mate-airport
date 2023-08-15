37b477cbc4b44f61214a60814bf9dc7d3bda987c
andy77andy <andy77andy@ukr.net>
2023-08-15 14:28:39 +0300

    Create Ticketserializer and add it to field tickets in OrderSerializer

--- airlines/serializers.py
+++ airlines/serializers.py
@@ -1,6 +1,7 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from airlines.models import Airplane, Crew, Flight, Route, Order
+from airlines.models import Airplane, Crew, Flight, Route, Order, Ticket
 
 
 class AirplaneSerializer(serializers.ModelSerializer):
@@ -27,7 +28,23 @@ class RouteSerializer(serializers.ModelSerializer):
         fields = ("id", "source", "destination",)
 
 
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination",)
+
+
 class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
     class Meta:
         model = Order
-        fields = ("id", "created_at", "user", )
+        fields = ("id", "tickets", "created_at", )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for tickets_data in tickets_data:
+                Ticket.objects.create(order=order, **tickets_data)
+            return order
