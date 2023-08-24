5e9d2032a9daa0e93afefab1c69ed564d72d6ee2
andy77andy <andy77andy@ukr.net>
2023-08-24 16:52:58 +0300

    Add test_airport_api toto checkperforming variuos actions

---
+++ airlines/tests/test_crew_api.py
@@ -0,0 +1,154 @@
+from datetime import datetime
+
+from django.db.models import F, Count
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from airlines.models import Airport, Route, Flight, Airplane, AirplaneType
+from airlines.serializers import FlightListSerializer, AirportDetailSerializer
+
+AIRPORT_URL = reverse("airlines:airport-list")
+
+
+def detail_url(airport_id: int):
+    return reverse("airlines:airport-detail", args=[airport_id])
+
+
+def sample_airport(**params):
+    defaults = {
+        "name": "Test",
+        "close_big_city": "Test city",
+    }
+    defaults.update(params)
+
+    return Airport.objects.create(**defaults)
+
+class AirportApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user("test@test1.com", "test1234")
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_airport_detail(self):
+        airport = sample_airport()
+
+        url = detail_url(airport.id)
+        response = self.client.get(url)
+        serializer = AirportDetailSerializer(airport)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_forbidden_for_not_admin(self):
+        payload = {
+            "name": "Test",
+            "close_big_city": "Test city",
+            }
+
+        response = self.client.post(AIRPORT_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class AdminAirportTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test1@test.com", "test1234", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_airport(self):
+        payload = {
+            "name": "Test",
+            "close_big_city": "Test city",
+        }
+
+        response = self.client.post(AIRPORT_URL, payload)
+
+        airport = Airport.objects.get(id=response.data["id"])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(payload["close_big_city"], getattr(airport, "close_big_city"))
+
+    def test_destroy_airport_not_allowed(self):
+        airport = sample_airport()
+        url = detail_url(airport.id)
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+# class AuthenticatedRouteApiTest(TestCase):
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = get_user_model().objects.create_user("test@test1.com", "test1234")
+#         self.client.force_authenticate(self.user)
+#
+#     def test_list_flight(self):
+#         airport1 = sample_airport(name="test1", close_big_city="Rome",)
+#         airport2 = sample_airport(name="test2", close_big_city="Lviv",)
+#         airport3 = sample_airport(name="test3", close_big_city="Riga", )
+#         airport4 = sample_airport(name="test4", close_big_city="Oslo", )
+#         route1 = Route.objects.create(source=airport1, destination=airport2)
+#         route2 = Route.objects.create(source=airport3, destination=airport4)
+#         airplane1 = sample_airplane()
+#         airplane2 = sample_airplane()
+#
+#         flight1 = Flight.objects.create(
+#             number="Test",
+#             route=route1,
+#             airplane=airplane1,
+#             departure_time="2020-10-08 18:00:00",
+#             arrival_time="2020-10-08 18:00:00"
+#         )
+#
+#         flight2 = Flight.objects.create(
+#             number="Test",
+#             route=route2,
+#             airplane=airplane2,
+#             departure_time=datetime(2023, 8, 21, 10, 30),
+#             arrival_time=datetime(2023, 8, 22, 10, 30),
+#         )
+#         flights = Flight.objects.annotate(
+#          tickets_available=(
+#                 F("airplane__rows") * F("airplane__seats_in_row")
+#                 - Count("tickets")
+#             )
+#         )
+#         """Create response from flight-list"""
+#         response = self.client.get(FLIGHT_URL)
+#         """Create response from flight-list with filtering"""
+#         response1 = self.client.get(FLIGHT_URL, {"route": "Riga"})
+#         response2 = self.client.get(FLIGHT_URL, {"date": "2023-08-21"})
+#
+#         serializer = FlightListSerializer(flights, many=True)
+#         serializer1 = FlightListSerializer(flights[1],)
+#         serializer2 = FlightListSerializer(flights[1],)
+#
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         self.assertEqual(response.data, serializer.data)
+#         self.assertIn(serializer1.data, response1.data)
+#         self.assertIn(serializer2.data, response2.data)
+
+# def sample_airplane_type(**params):
+#     defaults = {
+#         "name": "Test",
+#     }
+#     defaults.update(params)
+#
+#     return AirplaneType.objects.create(**defaults)
+#
+#
+# def sample_airplane(**params):
+#     defaults = {
+#         "name": "Test",
+#         "rows": 3,
+#         "seats_in_row": 10,
+#         "airplane_type": sample_airplane_type()
+#     }
+#     defaults.update(params)
+#
+#     return Airplane.objects.create(**defaults)
