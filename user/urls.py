38ba7e1d7460b8d73a902f5ca894ddfc3f276535
andy77andy <andy77andy@ukr.net>
2023-09-01 14:53:03 +0300

    Delete redundant comments

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
 
 from user.views import CreateUserView, ManageUserView
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
